c61087f1e32b4e649dece2b2b9713e015de07b48
Christopher Collins <qemtek@gmail.com>
2026-04-19 22:22:19 +0900

    feat: implement task(s) 121 [PARE-121]

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ import database
 app = Flask(__name__)
 
 
+@app.template_filter('format_score')
+def format_score(value):
+    return f"{int(value):,}"
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
